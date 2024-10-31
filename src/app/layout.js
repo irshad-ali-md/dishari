587ffd10c780d46f122fc91e6daf2ddf768f4317
Irshad <irshadali6541@gmail.com>
2024-10-30 22:22:11 -0500

    changed application name from Dishari to Drishti

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Dishari',
+  title: 'Drishti',
   description: 'A Novel Gesture-Based Educational Application for Specially Challenged People',
 }
 
